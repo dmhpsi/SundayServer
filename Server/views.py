20aa2071ae79b27bca27d426577df86b3b8b1a41
Minh Hieu Duong <thungthudh@gmail.com>
2019-06-05 09:08:25 +0700

    edit api_login

--- Server/views.py
+++ Server/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import User
 
@@ -14,11 +14,14 @@ def index(request):
 def api_login(request):
     if request.method == 'POST':
         body = json.loads(request.body)
-        username = body['username']
-        password = body['password']
-        user = User.objects.filter(username=username, password=password)
-        if len(user) > 0:
-            return HttpResponse('ok')
-        else:
-            return HttpResponse('fail')
+        try:
+            username = body['username']
+            password = body['password']
+            user = User.objects.filter(username=username, password=password)
+            if len(user) > 0:
+                return JsonResponse({'result': 'ok'})
+            else:
+                return JsonResponse({'result': 'fail'})
+        except KeyError:
+            return JsonResponse({'result': 'fail'})
     return HttpResponseForbidden()
